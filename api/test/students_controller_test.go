8d09e446d8eb604e47602953546dd0770c978855
matheusperezz <matheus.mperez08@gmail.com>
2024-01-29 18:02:20 -0300

    refactor on test update student

--- api/test/students_controller_test.go
+++ api/test/students_controller_test.go
@@ -3,11 +3,13 @@ package test
 import (
 	"bytes"
 	"encoding/json"
-	"github.com/go-playground/assert/v2"
 	"io"
 	"main/api/models"
 	"net/http"
+	"strconv"
 	"testing"
+
+	"github.com/go-playground/assert/v2"
 )
 
 func TestCreateStudent(t *testing.T) {
@@ -28,27 +30,42 @@ func TestCreateStudent(t *testing.T) {
 		}
 	}(resp.Body)
 
-	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	assert.Equal(t, http.StatusCreated, resp.StatusCode)
 }
 
 func TestUpdateStudent(t *testing.T) {
 	student := models.Student{
-		Name: "Updated Student",
+		Name: "Test Student",
 	}
 
 	jsonStudent, _ := json.Marshal(student)
-
-	req, err := http.NewRequest(http.MethodPut, "http://localhost:8080/students/1", bytes.NewBuffer(jsonStudent))
+	resp, err := http.Post("http://localhost:8080/students", "application/json", bytes.NewBuffer(jsonStudent))
 	if err != nil {
 		t.Fatalf("An error occured %v", err)
 	}
 
+	var createdStudent models.Student
+	json.NewDecoder(resp.Body).Decode(&createdStudent)
+
+	createdStudent.Name = "Updated Test Student"
+	endpoint := "http://localhost:8080/students/" + strconv.Itoa(int(createdStudent.ID))
+	jsonStudent, _ = json.Marshal(createdStudent)
+	req, err := http.NewRequest("PUT", endpoint, bytes.NewBuffer(jsonStudent))
+	if err != nil {
+		t.Fatalf("An error occured %v", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
 	client := &http.Client{}
-	resp, err := client.Do(req)
+	resp, err = client.Do(req)
 	if err != nil {
 		t.Fatalf("An error occured %v", err)
 	}
-	defer resp.Body.Close()
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			t.Fatalf("An error occured %v", err)
+		}
+	}(resp.Body)
 
 	assert.Equal(t, http.StatusOK, resp.StatusCode)
 }
